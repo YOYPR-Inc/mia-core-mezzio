1f77b345ae6f3f88492dd81b9c92cdc6c61813a2
Matias Camiletti <matias.camiletti@gmail.com>
2021-06-05 20:11:37 -0300

    Add tolower in create slug.

--- src/Helper/StringHelper.php
+++ src/Helper/StringHelper.php
@@ -35,6 +35,6 @@ class StringHelper
      */
     public static function createSlug($text)
     {
-        return preg_replace('/[^A-Za-z0-9-]+/', '-', $text);
+        return strtolower(preg_replace('/[^A-Za-z0-9-]+/', '-', $text));
     }
 }
